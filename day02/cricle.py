901209b10ebe3da9addf98b973fd9847188d9212
AlonGersh <agershkoviz@gmail.com>
2024-11-10 21:35:18 +0200

    Add files via upload

---
+++ day02/cricle.py
@@ -0,0 +1,9 @@
+import math
+
+radius = 4
+
+area = math.pi * (radius**2)
+circumference = 2 * (math.pi) * radius
+
+print("Circle area:", area)
+print("Circle circumference:", circumference)
---
+++ day02/rectangle.py
@@ -0,0 +1,8 @@
+width = 3  
+length = 2  
+
+area = width * length
+circumference = (width * 2) + (length * 2)
+
+print("Rectangle area:", area)
+print("Rectangle circumference:", circumference)
