8d3fd2b6bc7d529870e90d54bb89678970231470
AlonGerssh <agershkoviz@gmail.com>
2024-11-18 20:33:26 +0200

    circle argparse

---
+++ day03/circle.py
@@ -0,0 +1,15 @@
+import math
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--radius', help='Please enter the circle radius', required=True, type=int)
+
+args = parser.parse_args()
+
+radius = args.radius
+
+area = (math.pi) * (radius**2)
+circumference = 2 * (math.pi) * radius
+
+print("Circle area:", area)
+print("Circle circumference:", circumference)
