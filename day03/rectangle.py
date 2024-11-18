021fe95987d697b519d7b41bf824e3768d74e191
AlonGerssh <agershkoviz@gmail.com>
2024-11-18 19:05:06 +0200

    add input and int

---
+++ day03/rectangle.py
@@ -0,0 +1,8 @@
+width = int(input("Enter rectangle width: "))   
+length = int(input("Enter rectangle length: "))  
+
+area = width * length
+circumference = (width * 2) + (length * 2)
+
+print("Rectangle area:", area)
+print("Rectangle circumference:", circumference)
