f464f58127037d6a6717738b03e3619c80c5ca67
AlonGerssh <agershkoviz@gmail.com>
2024-12-03 19:57:03 +0200

    hw

---
+++ day05/sequences_and_statss.py
@@ -0,0 +1,7 @@
+for char in text:
+    if char.ischar():
+         number_of_letters
+        [int(char)] +=1
+print(number_of_letters)
+for i, count in enumerate(number_of_digit):
+    print(f"{i}: {count}")
