bbef6c20b158b8e129ee257efcf55ba3f079e128
AlonGerssh <agershkoviz@gmail.com>
2024-12-05 17:37:42 +0200

    hw

---
+++ day05/seq_test.py
@@ -0,0 +1,23 @@
+from seq import calculate_statistics
+
+def test_statistics()
+        res = calculate_statistics(a_seq.txt)
+        assert res == expected_result = {
+        "A": (2, 7.4),
+        "C": (5, 18.5),
+        "G": (6, 22.2),
+        "T": (7, 25.9),
+        "Unknown": (7, 25.9),
+        "Total": (27)
+    }
+
+def test_statistics()
+        res = calculate_statistics(b_seq.txt)
+        assert res == expected_result = {
+        "A": (1, 9.1),
+        "C": (2, 18.2),
+        "G": (3, 27.3),
+        "T": (4, 36.4),
+        "Unknown": (1, 9.1),
+        "Total": (11)
+    }
