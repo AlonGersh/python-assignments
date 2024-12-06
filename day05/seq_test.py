e98d5ac9edf0ad42c0fbcdd8aab7921113c1b383
AlonGerssh <agershkoviz@gmail.com>
2024-12-06 11:57:24 +0200

    hw

--- day05/seq_test.py
+++ day05/seq_test.py
@@ -1,23 +1,37 @@
 from seq import calculate_statistics
 
-def test_statistics()
-        res = calculate_statistics(a_seq.txt)
-        assert res == expected_result = {
+def test_statistics_a_seq():
+    # Read the content of a_seq.txt
+    with open("a_seq.txt", "r") as file:
+        sequence = file.read()
+
+    expected_result = {
         "A": (2, 7.4),
         "C": (5, 18.5),
         "G": (6, 22.2),
         "T": (7, 25.9),
         "Unknown": (7, 25.9),
-        "Total": (27)
     }
+    expected_total = 27
 
-def test_statistics()
-        res = calculate_statistics(b_seq.txt)
-        assert res == expected_result = {
+    stats, total = calculate_statistics(sequence)
+    assert stats == expected_result
+    assert total == expected_total
+
+def test_statistics_b_seq():
+    # Read the content of b_seq.txt
+    with open("b_seq.txt", "r") as file:
+        sequence = file.read()
+
+    expected_result = {
         "A": (1, 9.1),
         "C": (2, 18.2),
         "G": (3, 27.3),
         "T": (4, 36.4),
         "Unknown": (1, 9.1),
-        "Total": (11)
     }
+    expected_total = 11
+
+    stats, total = calculate_statistics(sequence)
+    assert stats == expected_result
+    assert total == expected_total
